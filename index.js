cad6626d4eb88e50dc957e2eb48810735c2ec037
disabledyou <85820729+disabledyou@users.noreply.github.com>
2024-02-16 22:05:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Eris = require("eris");
-const keep_alive = require('./keep_alive.js');
+const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
@@ -8,11 +8,12 @@ bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
 
-bot.on("ready", () => {
-  console.log(`${bot.user.username} is connected and ready!`);
-
-client.user.setActivity("stripped", {
-  type: "STREAMING",
-  url: "https://www.twitch.tv/air"
-});
 bot.connect(); // Get the bot to connect to Discord
+
+client.user.setPresence({
+    status: 'dnd',
+    activity: {
+        name: '/stripped',
+        type: 'PLAYING
+    }
+});
