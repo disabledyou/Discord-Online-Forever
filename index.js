8988c126b9d32c13d9591ff68c07bdf14a7c4d87
disabledyou <85820729+disabledyou@users.noreply.github.com>
2024-02-16 21:47:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Eris = require("eris");
-const keep_alive = require('./keep_alive.js')
+const keep_alive = require('./keep_alive.js');
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
@@ -8,14 +8,12 @@ bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
 
-bot.connect(); // Get the bot to connect to Discord
-
 bot.on("ready", () => {
   console.log(`${bot.user.username} is connected and ready!`);
 
   // Set streaming status
   bot.editStatus("streaming", {
-    name: "air",
+    name: "Streaming Game",
     type: 1, // 1 indicates streaming
     url: "www.youtube.com"
   });
